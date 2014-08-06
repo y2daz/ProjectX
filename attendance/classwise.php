bb9a41d4dab15c8ab249fe91191c9effd760450d
why-azdaan <why_azdaan@yahoo.com>
2014-08-06 19:03:49 +0530

    Added Student-wise to Template

--- attendance/classwise.php
+++ attendance/classwise.php
@@ -8,74 +8,67 @@
 ob_start();
 ?>
 <html>
-<head>
-    <style type=text/css>
-        #main{ height:<?php echo "$fullPageHeight" . "px";?> }
-        #footer{ top:<?php echo "$footerTop" . "px";?> }
+    <head>
+        <style type=text/css>
+            #main{ height:<?php echo "$fullPageHeight" . "px";?> }
+            #footer{ top:<?php echo "$footerTop" . "px";?> }
 
-        h1{
-            text-align: center;
-        }
-        .insert{
-            position:absolute;
-            left:40px;
-            top:80px;
-        }
-        .insert th{
-            color:white;
-            background-color: #005e77;
-            height:30px;
-            padding:5px;
-        }
-        .insert input.button{
-            position:relative;
-            font-weight:bold;
-            font-size:15px;
-            Right:-335px;
-            top:20px;
-        }
-        .insert input{
-            position:relative;
-            font-weight:bold;
-            font-size:15px;
-            Right:-335px;
-            top: 355px;
-        }
-        .insert td{
-            padding:10px;
-        }
-    </style>
-</head>
-<body>
+            h1{
+                text-align: center;
+            }
+            .insert{
+                position:absolute;
+                left:40px;
+                top:80px;
+            }
+            .insert th{
+                color:white;
+                background-color: #005e77;
+                height:30px;
+                padding:5px;
+            }
+            .insert input.button{
+                position:relative;
+                font-weight:bold;
+                font-size:15px;
+                Right:-335px;
+                top:20px;
+            }
+            .insert input{
+                font-weight:bold;
+                font-size:15px;
+            }
+            .insert td{
+                padding:10px;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>Class-wise Attendance Report</h1>
 
+        <form>
 
-    <h1>Class Wise Attendance Report</h1>
-
-    <form>
-
-        <table class="insert" cellspacing="0">
-            <tr><th>Class wise details</th><th></th></tr>
-            <tr>
-
-                <td>Grade</td>
-                <td><input type="text" value=""></td>
-                <td>Class</td>
-                <td><input type="text" value=""></td>
-
-            </tr>
-            <tr>
-                <td>Date from</td>
-                <td><input type="date" value=""></td>
-                <td>Date To</td>
-                <td><input type="date" value=""></td>
-            </tr>
-            <tr>
-                <td></td>
-                <td><input class="button" type="button" value="Submit"></td>
-            </tr>
-        </table>
-    </form>
-</body>
+            <table class="insert" cellspacing="0">
+                <tr><th>Class-wise Report</th><th></th></tr>
+                <tr>
+                    <td>Grade</td>
+                    <td><input name="studentGrade" type="text" value=""></td>
+                    <td>Class</td>
+                    <td><input name="studentClass" type="text" value=""></td>
+                </tr>
+                <tr>
+                    <td>Date from</td>
+                    <td><input type="date" value=""></td>
+                    <td>Date To</td>
+                    <td><input type="date" value=""></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td><input class="button" type="button" value="Submit"></td>
+                </tr>
+            </table>
+        </form>
+    </body>
 </html>
 <?php
 //Assign all Page Specific variables
