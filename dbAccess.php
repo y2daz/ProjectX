0c658acafb3b4bc9b044bc6f8a7ee1e2b117aca8
why-azdaan <why_azdaan@yahoo.com>
2014-08-06 17:50:34 +0530

    Minor changes to dbAccess

--- dbAccess.php
+++ dbAccess.php
@@ -5,22 +5,16 @@
  * Date: 8/5/14
  * Time: 12:00 PM
  */
-    require_once("dbConnect.php");
+
+/**
+ * This is where all the sql commands are kept
+ *
+ */
+
+
+require_once("dbConnect.php");
 
     $dbObj = new dbConnect();
-
-//    $mysqli = $dbObj->getConnection();
-
-//    if ($mysqli->connect_errno) {
-//        die ("Failed to connect to MySQL: " . $mysqli->connect_error );
-//    }
-//
-//    $result = $mysqli->query("SELECT 'The SQL Works, people.' AS _msg");
-//    $row = $result->fetch_assoc();
-//
-//    echo $row['_msg'];
-//
-//    $mysqli->close();
 
     function login($username, $password)
     {
