1aaba163810bd8695afa0f73bc5c96b03aaa8df7
raZor1993 <tmadusha93@gmail.com>
2014-08-07 15:04:39 +0530

    Changes to sql

--- manaDB.sql
+++ manaDB.sql
@@ -18,6 +18,12 @@ USE `manaDB`;
 
 CREATE USER 'manaSystem'@'localhost' IDENTIFIED BY 'SMevHZxMEJVfv4Kc';
 
+GRANT ALL PRIVILEGES ON * . * TO 'manaSystem'@'localhost';
+	
+GRANT ALL PRIVILEGES ON manaDB.* TO 'manaSystem'@'localhost';
+
+FLUSH PRIVILEGES;
+
 use manaDB;
 
 CREATE TABLE User(
@@ -30,3 +36,58 @@ Use manaDB;
 INSERT INTO User values ('y', (SELECT PASSWORD('a')));
 INSERT INTO User values ('a', (SELECT PASSWORD('a')));
 
+
+
+CREATE TABLE Staff(
+  StaffID varchar(5) primary key,
+  NamewithInitials varchar(60),
+  DateofBirth date,
+  Gender INTEGER,
+  Nationality_Race INTEGER,
+  Religion Integer,
+  CivilStatus INTEGER,
+  NICNumber varchar(10),
+  MailDeliveryAddress varchar(100),
+  ContactNumber varchar(15),
+  DateAppointedastoPost DATE, /*Appointed as Teacher/Principal*/
+  DateJoinedthisSchool Date,
+  EmploymentStatus Integer,
+  Medium Integer,
+  PositioninSchool Integer,
+  Section Integer,
+  SubjectMostTaught Integer,
+  SubjectSecondMostTaught Integer,
+  ServiceGrade Integer, /*Grade at Sri Lanka Teacher Association/ Principal Association, etc.*/
+  Salary float,
+  HighestEducationalQualification Integer,
+  HighestProfessionalQualification Integer,
+  CourseofStudy Integer
+);
+
+CREATE TABLE ClassInformation(
+  StaffID varchar(5),
+  Grade INTEGER,
+  Class char(2),
+
+  FOREIGN KEY fk001 (StaffID) REFERENCES Staff(StaffID),
+  PRIMARY KEY (Grade, Class)
+);
+
+/*SPORTS ACHIEVEMENTS*/
+
+CREATE TABLE Blacklist(
+  StaffID varchar(5),
+  ListContributor varchar(5),
+  EnterDate DATE,
+  Reason varchar(255),
+
+  FOREIGN KEY fk002 (StaffID) REFERENCES Staff(StaffID),
+  FOREIGN KEY fk003 (ListContributor) REFERENCES Staff(StaffID),
+
+  PRIMARY KEY (StaffID, ListContributor, EnterDate)
+);
+
+
+
+
+CREATE TABLE LabelLanguage();
