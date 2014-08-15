4b99d4d383a0638cd3c41a7d375daa1f31643698
y2daz <yazdaan.alimudeen@gmail.com>
2014-08-15 22:36:06 +0530

    ALInput changes

--- marksAndGrading/ALinput.php
+++ marksAndGrading/ALinput.php
@@ -12,217 +12,164 @@ ob_start();
 
 ?>
 <html>
-<head>
-<style type=text/css>
-<!--            #main{ height:--><?php //echo "$fullPageHeight" . "px";?><!-- }-->
-<!--            #footer{ top:--><?php //echo "$footerTop" . "px";?><!-- }-->
+    <head>
+        <style type=text/css>
+        <!--            #main{ height:--><?php //echo "$fullPageHeight" . "px";?><!-- }-->
+        <!--            #footer{ top:--><?php //echo "$footerTop" . "px";?><!-- }-->
 
 
-    table, th, td {
-        border: 0px solid black;
+            table, th, td {
+                border: 0px solid black;
 
-    }
-    th{
-        width: 1000px;
-        text-align:left;
-    }
+            }
+            th{
+                width: 1000px;
+                text-align:left;
+            }
 
-    td {
-        width: 10px;
-        text-align:left;
-    }
+            td {
+                width: 10px;
+                text-align:left;
+            }
 
-    tr{
-        height: 20px;
-    }
-
-
-
-    input.button {
-        position:relative;
-        font-weight:bold;
-        font-size:20px;
-        left:30px;
-        top:20px;
-    }
-    input.button1 {
-        position:relative;
-        font-weight:bold;
-        font-size:20px;
-        left:50px;
-        top:20px;
-    }
-
-
-
-</style>
+            tr{
+                height: 20px;
+            }
+            input.button {
+                position:relative;
+                font-weight:bold;
+                font-size:20px;
+                left:30px;
+                top:20px;
+            }
+            input.button1 {
+                position:relative;
+                font-weight:bold;
+                font-size:20px;
+                left:50px;
+                top:20px;
+            }
+        </style>
     </head>
-
-
 <body>
 
 
     <h1>G.C.E A/L Results Sheet</h1>
-<tr>
-    <td>Index Number</td>
-    <td><input type="text" value=""></td>
-    <td>Name</td>
-    <td><input type="text" value="" </td>
-    <td>Year</td>
-    <td><input type=text" value=""</td>
-</tr>
-<h2></h2>
 
-<table   border="1px solid black" >
-
-
+    <table>
     <tr>
-        <th>Subject</th>
-        <th>Grade</th>
-        <th>Medium</th>
-
+        <td>Index Number</td>
+        <td><input type="text" value=""></td>
+        <td>Name</td>
+        <td><input type="text" value="" </td>
+        <td>Year</td>
+        <td><input type=text" value=""</td>
     </tr>
-    <tr>
-        <td><input type="text" value="">
-        <td><select type="text" value="">
+    </table>
 
-                <option>--</option>
-                <option>A</option>
-                <option>B</option>
-                <option>C</option>
-                <option>S</option>
-                <option>F</option>
-                <td><select type="text" value="">
+    <br />
 
-                        <option>--</option>
-                        <option>S</option>
-                        <option>E</option>
-                        <option>T</option>
-                    </select>
-    </tr>
-    <tr>
-        <td><input type="text" value="">
-        <td><select type="text" value="">
+    <table>
+        <tr>
+            <th>Subject</th>
+            <th>Grade</th>
+        </tr>
+        <tr>
+            <td><input type="text" value=""></td>
+            <td><select type="text" value="">
+                    <option>--</option>
+                    <option>A</option>
+                    <option>B</option>
+                    <option>C</option>
+                    <option>S</option>
+                    <option>F</option>
+                </select>
+            </td>
+        </tr>
+        <tr>
+            <td><input type="text" value=""></td>
+            <td><select type="text" value="">
+                    <option>--</option>
+                    <option>A</option>
+                    <option>B</option>
+                    <option>C</option>
+                    <option>S</option>
+                    <option>F</option>
+                </select>
+            </td>
+        </tr>
+        <tr>
+            <td><input type="text" value=""></td>
+            <td><select type="text" value="">
+                    <option>--</option>
+                    <option>A</option>
+                    <option>B</option>
+                    <option>C</option>
+                    <option>S</option>
+                    <option>F</option>
+                </select>
+            </td>
+        </tr>
+        <tr>
+            <td>General English</td>
+            <td><select type="text" value="">
 
-                <option>--</option>
-                <option>A</option>
-                <option>B</option>
-                <option>C</option>
-                <option>S</option>
-                <option>F</option>
-                <td><select type="text" value="">
-
-                        <option>--</option>
-                        <option>S</option>
-                        <option>E</option>
-                        <option>T</option>
-                    </select>
-    </tr>
-
-    <tr>
-        <td><input type="text" value="">
-        <td><select type="text" value="">
-
-                <option>--</option>
-                <option>A</option>
-                <option>B</option>
-                <option>C</option>
-                <option>S</option>
-                <option>F</option>
-                <td><select type="text" value="">
-
-                        <option>--</option>
-                        <option>S</option>
-                        <option>E</option>
-                        <option>T</option>
-                    </select>
-    </tr>
-
-    <tr>
-
-        <td>General English</td>
-        <td><select type="text" value="">
-
-                <option>--</option>
-                <option>A</option>
-                <option>B</option>
-                <option>C</option>
-                <option>S</option>
-                <option>F</option>
-                <td><select type="text" value="">
-
-                        <option>--</option>
-
-                        <option>E</option>
-
-                    </select>
-    </tr>
-
-    <tr>
-
-        <td>Common General Test</td>
-        <td><select type="text" value="">
-
-                <option>--</option>
-                <option>A</option>
-                <option>B</option>
-                <option>C</option>
-                <option>S</option>
-                <option>F</option>
-                <td><select type="text" value="">
-
-                        <option>--</option>
-                        <option>S</option>
-                        <option>E</option>
-                        <option>T</option>
-                    </select>
-    </tr>
-
-</table>
+                    <option>--</option>
+                    <option>A</option>
+                    <option>B</option>
+                    <option>C</option>
+                    <option>S</option>
+                    <option>F</option>
+                </select>
+            </td>
+        </tr>
+        <tr>
+            <td>Common General Test</td>
+            <td><select type="text" value="">
+                    <option>--</option>
+                    <option>A</option>
+                    <option>B</option>
+                    <option>C</option>
+                    <option>S</option>
+                    <option>F</option>
+                </select>
+            </td>
+        </tr>
+    </table>
     <form>
-    <h3></h3>
-    <tr class="alt">
+        <br />
 
-        <td>Z-Score</td>
-        <td><input type="text" value="">
-        </td>
-    </tr>
+        <table>
+                <tr>
+                    <td>Z-Score</td>
+                    <td><input type="text" value="">
+                    </td>
+                </tr>
+                <tr>
+                    <td>Island Rank</td>
+                    <td><input type="text" value="">
+                    </td>
+                </tr>
+                <tr>
+                    <td>District Rank</td>
+                    <td><input type="text" value="">
+                    </td>
+                </tr>
+        </table>
+    </form>
 
-    <tr class="alt">
-
-        <td>Inland Rank</td>
-        <td><input type="text" value="">
-        </td>
-    </tr>
-
-    <tr class="alt">
-
-        <td>District Rank</td>
-        <td><input type="text" value="">
-        </td>
-    </tr>
-        </form>
-
-
-    <tr>
-    <td></td>
-
-    <td><input class="button" type="submit" value="Submit"></td>
-
-
-
-
-    <td><input class="button1" type="reset" value="Reset"></td>
-
-
-
-</tr>
-
+    <table>
+        <tr>
+            <td></td>
+            <td><input class="button" type="submit" value="Submit"></td>
+            <td><input class="button1" type="reset" value="Reset"></td>
+        </tr>
+    </table>
 </body>
 </html>
 <?php
 //Change these to what you want
-$fullPageHeight = 1000;
+$fullPageHeight = 700;
 $footerTop = $fullPageHeight + 100;
 $pageTitle= "Template";
 //Only change above
