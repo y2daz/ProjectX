08ec399fb5517b5b87c39751177c35127af503d5
shavin47 <shavin47@hotmail.com>
2014-08-17 21:48:14 +0530

    tiny change

--- dbAccess.php
+++ dbAccess.php
@@ -540,5 +540,18 @@ function insertblacklist($staffID, $listcontributor, $enterdate, $reason)
 
     }
 
+    function approveLeave()
+    {
+        $dbObj = new dbConnect();
+        $mysqli = $dbObj->getConnection();
+
+        if ($mysqli->connect_errno) {
+
+            die ("Failed to connect to MySQL: " . $mysqli->connect_error );
+        }
+
+
+    }
+
 
 ?>
