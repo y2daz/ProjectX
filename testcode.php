81ddaaecff38dd31e2be553e7e9fb177ec7b9e0f
shavin47 <shavin47@hotmail.com>
2014-08-23 17:44:23 +0530

    testing stuff

--- testcode.php
+++ testcode.php
@@ -6,4 +6,32 @@
  * Time: 10:50 PM
  */
 
-echo date("Y-M-D");
+?>
+
+
+<html>
+
+<title> Header </title>
+
+<head>
+
+    <script type="text/javascript" src="common.js">
+
+    </script>
+
+
+</head>
+
+<body>
+
+    <?php
+
+        $testcode = "Shavin Peiris";
+
+    ?>
+
+    <input type="button" value="Testing" onclick="sendMessage(<?php echo $testcode ?>)"
+
+</body>
+
+</html>
