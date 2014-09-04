cb1edcb6accd3d280b21ca599a3a9d0c8c43e4cc
y2daz <yazdaan.alimudeen@gmail.com>
2014-09-04 23:28:45 +0530

    Timetable Class.

---
+++ TimeTable/timetableClass.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yazdaan
+ * Date: 04/09/14
+ * Time: 22:37
+ */
+
+    require_once("../dbConnect.php");
+    require_once("../formValidation.php");
+
+
+class Timetable {
+
+    public $slot = array();
+    public $staffId;
+
+    function __construct(){
+        for ($i = 0; $i < 40; $i++)
+        {
+            $slot[$i] = new SlotDetail();
+            $a = new SlotDetail();
+
+        }
+    }
+
+    public function getTimetable(){
+//        $dbObj = new dbConnect();
+//        $mysqli = $dbObj->getConnection();
+//
+//        if ($mysqli->connect_errno) {
+//            die ("Failed to connect to MySQL: " . $mysqli->connect_error );
+//        }
+//
+//        if ($stmt = $mysqli->prepare("Select userPassword, accessLevel FROM User WHERE userEmail=? AND isDeleted=0 LIMIT 1;"))
+//        {
+//            $stmt -> bind_param("s", $email);
+//
+//            if ($stmt->execute())
+//            {
+////                $stmt->store_result();
+//
+//                $OUTuserPassword = NULL;
+//                $OUTaccessLevel = 0;
+//
+//                $stmt->bind_result($OUTuserPassword, $OUTaccessLevel);
+//                $stmt->fetch();
+//
+////                echo $password . " " . $OUTuserPassword . "\n";
+////                echo password_hash($password, PASSWORD_DEFAULT);
+//
+////                $hash=password_hash("rasmuslerdorf", PASSWORD_DEFAULT);
+//
+//                if (password_verify($password, $OUTuserPassword))
+//                {
+//                    $_SESSION["user"]="$email";
+//                    $stmt->close();
+//                    $mysqli->close();
+//                    return true;
+//                }
+//            }
+//        }
+//        $stmt->close();
+//        $mysqli->close();
+//        return false;
+    }
+
+    public function setTimetable( $array ){
+        //Probably 2d array. Associative.
+
+    }
+
+    public function dbUpdate(){
+
+    }
+
+    private function insertSLot($number, $grade, $class, $subject)
+    {
+        $this->slot[$number]->sGrade = $grade;
+        $this->slot[$number]->sClass = $class;
+        $this->slot[$number]->sSubject = $subject;
+    }
+
+
+}
+
+class SlotDetail {
+    public $sGrade;
+    public $sClass;
+    public $sSubject;
+}
