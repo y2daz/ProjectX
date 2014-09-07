e08ddcca8462e419d4c0ca4961988ed94fa33c42
Ish777 <isurujkd@gmail.com>
2014-09-07 15:20:36 +0530

    added language data to leave management

--- studentInformation/studentRegistration.php
+++ studentInformation/studentRegistration.php
@@ -173,7 +173,7 @@ if (isset($_POST["submit"])) //User has clicked the submit button to add a stude
             <tr>
                 <td></td>
 
-                <td><input name="submit" class="button1" type="submit" value="Submit" onclick="window.location = 'parentDetailsForm.php'" required="true"></td>
+                <td><input name="submit" class="button1" type="submit" value="Submit" required="true"></td>
 
 
             </tr>
