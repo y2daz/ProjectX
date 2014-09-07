6ad1eff3aacb65e7d62b0ec0538ddbd6d63cdc52
y2daz <yazdaan.alimudeen@gmail.com>
2014-09-07 22:12:55 +0530

    New database with leaveData for existing staff.

--- manaDB.sql
+++ manaDB.sql
@@ -13,7 +13,7 @@
   --
   -- Database: `manaDB`
   --
-  DROP DATABASE IF EXISTS `manadb`;
+  DROP DATABASE IF EXISTS `manaDB`;
 
 #   CREATE USER 'manaSystem'@'localhost' IDENTIFIED BY 'SMevHZxMEJVfv4Kc';
 #
@@ -26,24 +26,25 @@
 #   GRANT SELECT, FILE, CREATE TEMPORARY TABLES, CREATE VIEW, SHOW VIEW ON manaDB.* TO 'manaReporting'@'localhost' IDENTIFIED BY 'eveA0njJ7pYsFC1M' WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0;
 #
 #   FLUSH PRIVILEGES;
+
 -- phpMyAdmin SQL Dump
--- version 4.1.14
+-- version 4.0.10deb1
 -- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: Sep 07, 2014 at 08:19 AM
--- Server version: 5.5.37
--- PHP Version: 5.5.12
+-- Host: localhost
+-- Generation Time: Sep 07, 2014 at 10:10 PM
+-- Server version: 5.5.38-0ubuntu0.14.04.1
+-- PHP Version: 5.5.9-1ubuntu4.3
 
   SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+  SET AUTOCOMMIT = 0;
+  START TRANSACTION;
   SET time_zone = "+00:00";
 
 --
--- Database: `manadb`
+-- Database: `manaDB`
 --
-DROP DATABASE IF EXISTS manaDB;
-
-  CREATE DATABASE IF NOT EXISTS `manaDB` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE DATABASE IF NOT EXISTS `manaDB` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
   USE `manaDB`;
 
 -- --------------------------------------------------------
@@ -803,6 +804,27 @@ DROP DATABASE IF EXISTS manaDB;
     PRIMARY KEY (`StaffID`)
   ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `LeaveData`
+--
+
+  INSERT INTO `LeaveData` VALUES('1', 15, 90, 15, 0);
+  INSERT INTO `LeaveData` VALUES('10', 15, 90, 15, 0);
+  INSERT INTO `LeaveData` VALUES('11', 15, 90, 15, 0);
+  INSERT INTO `LeaveData` VALUES('12', 15, 90, 15, 0);
+  INSERT INTO `LeaveData` VALUES('13', 15, 90, 15, 0);
+  INSERT INTO `LeaveData` VALUES('14', 15, 90, 15, 0);
+  INSERT INTO `LeaveData` VALUES('15', 15, 90, 15, 0);
+  INSERT INTO `LeaveData` VALUES('16', 15, 90, 15, 0);
+  INSERT INTO `LeaveData` VALUES('2', 15, 90, 15, 0);
+  INSERT INTO `LeaveData` VALUES('3', 15, 90, 15, 0);
+  INSERT INTO `LeaveData` VALUES('4', 15, 90, 15, 0);
+  INSERT INTO `LeaveData` VALUES('5', 15, 90, 15, 0);
+  INSERT INTO `LeaveData` VALUES('6', 15, 90, 15, 0);
+  INSERT INTO `LeaveData` VALUES('7', 15, 90, 15, 0);
+  INSERT INTO `LeaveData` VALUES('8', 15, 90, 15, 0);
+  INSERT INTO `LeaveData` VALUES('9', 15, 90, 15, 0);
+
 -- --------------------------------------------------------
 
 --
@@ -918,29 +940,29 @@ DROP DATABASE IF EXISTS manaDB;
 -- Dumping data for table `Student`
 --
 
-  INSERT INTO `Student` (`AdmissionNo`, `NameWithInitials`, `DateOfBirth`, `Nationality_Race`, `Religion`, `Medium`, `Address`, `Grade`, `Class`, `House`, `isDeleted`) VALUES
-    ('1', 'Madusha', '2000-12-12', 1, 1, 1, 'Here', 12, 'B', 'Blue', 0),
-    ('12325', 'B.T.M Mendia', '1993-12-01', 1, 1, 1, 'Moratuwa', 11, 'A', 'Shantha', 0),
-    ('12345', 'J.A.I Jayakody', '1993-12-31', 1, 1, 1, 'Colombo', 11, 'A', 'Surya', 0),
-    ('2', 'Peiris M.S.E', '1993-09-03', 1, 1, 1, 'kolpity', 11, 'C', 'Green', 0),
-    ('23247', 'Sanchayan Balayan', '1991-01-01', 1, 1, 1, 'Wellawattha', 11, 'B', 'Surya', 0),
-    ('23415', 'Upeka tisser', '1990-12-23', 1, 1, 1, 'Wadduwa', 11, 'B', 'Surya', 0),
-    ('24242', 'Yazdaan Mohamed', '1995-12-30', 4, 3, 1, 'Nugegoda', 11, 'B', 'Shantha', 0),
-    ('3', 'jaya', '1993-01-21', 1, 1, 1, 'kolpity', 11, 'A', 'Blue', 0),
-    ('32424', 'sahan tissera', '1991-12-02', 1, 1, 1, 'Angoda', 11, 'B', 'Shantha', 0),
-    ('43435', 'Venusha Ranaviraj', '1993-12-21', 1, 1, 1, 'Ganemulla', 11, 'A', 'veera', 0),
-    ('43536', 'Dulip Rathnayaka', '1991-12-31', 1, 1, 1, 'Kottawa', 11, 'A', 'Su', 0),
-    ('43568', 'M.C Liyanage', '1994-12-31', 1, 1, 1, 'Borella', 11, 'B', 'veera', 0),
-    ('45678', 'Janith Heshan', '1989-12-31', 1, 1, 1, 'Rajagiriya', 11, 'A', 'Surya', 0),
-    ('54645', 'Darsha Fonseka', '1989-12-31', 1, 1, 1, 'kolpity', 11, 'B', 'Methta', 0),
-    ('54646', 'Devni Indula', '1991-11-28', 1, 1, 1, 'Panadura', 11, 'B', 'Surya', 0),
-    ('64558', 'Niruthi Yogalingam', '1995-12-30', 1, 1, 1, 'negombo', 11, 'B', 'Shantha', 0),
-    ('65578', ' Sampath Jayasundara', '1991-12-01', 1, 1, 1, 'negombo', 11, 'A', 'Methta', 0),
-    ('65758', 'Ashan Asela', '2005-01-31', 1, 1, 1, 'Dehiwala', 11, 'B', 'Surya', 0),
-    ('65784', 'Mevanka Roshali', '1993-11-30', 1, 1, 1, 'Dehiwala', 11, 'A', 'veera', 0),
-    ('75638', 'Jane Arathy', '1993-11-29', 1, 1, 1, 'Kotahena', 11, 'B', 'Methta', 0),
-    ('75857', 'Gihan Jayawardena', '1988-12-31', 1, 1, 1, 'kolpity', 11, 'A', 'Methta', 0),
-    ('76954', 'Raneesha Peiries', '1994-12-31', 1, 1, 1, 'Wellawattha', 11, 'A', 'Shantha', 0);
+  INSERT INTO `Student` VALUES('1', 'Madusha', '2000-12-12', 1, 1, 1, 'Here', 12, 'B', 'Blue', 0);
+  INSERT INTO `Student` VALUES('12325', 'B.T.M Mendia', '1993-12-01', 1, 1, 1, 'Moratuwa', 11, 'A', 'Shantha', 0);
+  INSERT INTO `Student` VALUES('12345', 'J.A.I Jayakody', '1993-12-31', 1, 1, 1, 'Colombo', 11, 'A', 'Surya', 0);
+  INSERT INTO `Student` VALUES('2', 'Peiris M.S.E', '1993-09-03', 1, 1, 1, 'kolpity', 11, 'C', 'Green', 0);
+  INSERT INTO `Student` VALUES('23247', 'Sanchayan Balayan', '1991-01-01', 1, 1, 1, 'Wellawattha', 11, 'B', 'Surya', 0);
+  INSERT INTO `Student` VALUES('23415', 'Upeka tisser', '1990-12-23', 1, 1, 1, 'Wadduwa', 11, 'B', 'Surya', 0);
+  INSERT INTO `Student` VALUES('24242', 'Yazdaan Mohamed', '1995-12-30', 4, 3, 1, 'Nugegoda', 11, 'B', 'Shantha', 0);
+  INSERT INTO `Student` VALUES('3', 'jaya', '1993-01-21', 1, 1, 1, 'kolpity', 11, 'A', 'Blue', 0);
+  INSERT INTO `Student` VALUES('32424', 'sahan tissera', '1991-12-02', 1, 1, 1, 'Angoda', 11, 'B', 'Shantha', 0);
+  INSERT INTO `Student` VALUES('43435', 'Venusha Ranaviraj', '1993-12-21', 1, 1, 1, 'Ganemulla', 11, 'A', 'veera', 0);
+  INSERT INTO `Student` VALUES('43536', 'Dulip Rathnayaka', '1991-12-31', 1, 1, 1, 'Kottawa', 11, 'A', 'Su', 0);
+  INSERT INTO `Student` VALUES('43568', 'M.C Liyanage', '1994-12-31', 1, 1, 1, 'Borella', 11, 'B', 'veera', 0);
+  INSERT INTO `Student` VALUES('45678', 'Janith Heshan', '1989-12-31', 1, 1, 1, 'Rajagiriya', 11, 'A', 'Surya', 0);
+  INSERT INTO `Student` VALUES('54645', 'Darsha Fonseka', '1989-12-31', 1, 1, 1, 'kolpity', 11, 'B', 'Methta', 0);
+  INSERT INTO `Student` VALUES('54646', 'Devni Indula', '1991-11-28', 1, 1, 1, 'Panadura', 11, 'B', 'Surya', 0);
+  INSERT INTO `Student` VALUES('64558', 'Niruthi Yogalingam', '1995-12-30', 1, 1, 1, 'negombo', 11, 'B', 'Shantha', 0);
+  INSERT INTO `Student` VALUES('65578', ' Sampath Jayasundara', '1991-12-01', 1, 1, 1, 'negombo', 11, 'A', 'Methta', 0);
+  INSERT INTO `Student` VALUES('65758', 'Ashan Asela', '2005-01-31', 1, 1, 1, 'Dehiwala', 11, 'B', 'Surya', 0);
+  INSERT INTO `Student` VALUES('65784', 'Mevanka Roshali', '1993-11-30', 1, 1, 1, 'Dehiwala', 11, 'A', 'veera', 0);
+  INSERT INTO `Student` VALUES('75638', 'Jane Arathy', '1993-11-29', 1, 1, 1, 'Kotahena', 11, 'B', 'Methta', 0);
+  INSERT INTO `Student` VALUES('75857', 'Gihan Jayawardena', '1988-12-31', 1, 1, 1, 'kolpity', 11, 'A', 'Methta', 0);
+  INSERT INTO `Student` VALUES('76954', 'Raneesha Peiries', '1994-12-31', 1, 1, 1, 'Wellawattha', 11, 'A', 'Shantha', 0);
+
 -- --------------------------------------------------------
 
 --
@@ -957,10 +979,10 @@ DROP DATABASE IF EXISTS manaDB;
 -- --------------------------------------------------------
 
 --
--- Table structure for table `subject_grade`
+-- Table structure for table `Subject_Grade`
 --
 
-  CREATE TABLE IF NOT EXISTS `subject_grade` (
+  CREATE TABLE IF NOT EXISTS `Subject_Grade` (
     `SubjectID` int(11) NOT NULL DEFAULT '0',
     `Grade` int(11) DEFAULT NULL,
     `Subject` varchar(30) DEFAULT NULL,
@@ -985,10 +1007,10 @@ DROP DATABASE IF EXISTS manaDB;
 -- --------------------------------------------------------
 
 --
--- Table structure for table `termmarks`
+-- Table structure for table `TermMarks`
 --
 
-  CREATE TABLE IF NOT EXISTS `termmarks` (
+  CREATE TABLE IF NOT EXISTS `TermMarks` (
     `AdmissionNo` varchar(5) NOT NULL DEFAULT '',
     `Subject` varchar(64) NOT NULL DEFAULT '0',
     `Term` int(11) NOT NULL DEFAULT '0',
@@ -1661,11 +1683,11 @@ DROP DATABASE IF EXISTS manaDB;
   INSERT INTO `Timetable` VALUES(NULL, NULL, 4, 6, NULL, '9', 0);
   INSERT INTO `Timetable` VALUES(NULL, NULL, 4, 7, NULL, '9', 0);
 
-  -- --------------------------------------------------------
+-- --------------------------------------------------------
 
-  --
-  -- Table structure for table `Transaction`
-  --
+--
+-- Table structure for table `Transaction`
+--
 
   CREATE TABLE IF NOT EXISTS `Transaction` (
     `EventID` int(11) NOT NULL DEFAULT '0',
@@ -1740,10 +1762,10 @@ ALTER TABLE `ApplyLeave`
   ADD CONSTRAINT `ApplyLeave_ibfk_1` FOREIGN KEY (`StaffID`) REFERENCES `Staff` (`StaffID`),
   ADD CONSTRAINT `ApplyLeave_ibfk_2` FOREIGN KEY (`ReviewedBy`) REFERENCES `Staff` (`StaffID`);
 
-  --
-  -- Constraints for table `Attendance`
-  --
-  ALTER TABLE `Attendance`
+--
+-- Constraints for table `Attendance`
+--
+ALTER TABLE `Attendance`
   ADD CONSTRAINT `Attendance_ibfk_1` FOREIGN KEY (`AdmissionNo`) REFERENCES `Student` (`AdmissionNo`);
 
 --
@@ -1764,10 +1786,10 @@ ALTER TABLE `ClassInformation`
 ALTER TABLE `Event`
   ADD CONSTRAINT `Event_ibfk_1` FOREIGN KEY (`EventCreator`) REFERENCES `Staff` (`StaffID`);
 
---
--- Constraints for table `EventManager`
---
-ALTER TABLE `EventManager`
+  --
+  -- Constraints for table `EventManager`
+  --
+  ALTER TABLE `EventManager`
   ADD CONSTRAINT `EventManager_ibfk_1` FOREIGN KEY (`EventID`) REFERENCES `Event` (`EventID`),
   ADD CONSTRAINT `EventManager_ibfk_2` FOREIGN KEY (`StaffID`) REFERENCES `Staff` (`StaffID`);
 
@@ -1804,28 +1826,28 @@ ALTER TABLE `LanguageOption`
   ADD CONSTRAINT `LanguageOption_ibfk_1` FOREIGN KEY (`GroupNo`) REFERENCES `LanguageGroup` (`GroupNo`),
   ADD CONSTRAINT `LanguageOption_ibfk_2` FOREIGN KEY (`Language`) REFERENCES `Language` (`Language`);
 
-  --
-  -- Constraints for table `LeaveData`
-  --
-  ALTER TABLE `LeaveData`
+--
+-- Constraints for table `LeaveData`
+--
+ALTER TABLE `LeaveData`
   ADD CONSTRAINT `LeaveData_ibfk_1` FOREIGN KEY (`StaffID`) REFERENCES `Staff` (`StaffID`);
 
-  --
-  -- Constraints for table `OLMarks`
-  --
-  ALTER TABLE `OLMarks`
+--
+-- Constraints for table `OLMarks`
+--
+ALTER TABLE `OLMarks`
   ADD CONSTRAINT `OLMarks_ibfk_1` FOREIGN KEY (`AdmissionNo`) REFERENCES `Student` (`AdmissionNo`);
 
-  --
-  -- Constraints for table `ParentsInformation`
-  --
-  ALTER TABLE `ParentsInformation`
+--
+-- Constraints for table `ParentsInformation`
+--
+ALTER TABLE `ParentsInformation`
   ADD CONSTRAINT `ParentsInformation_ibfk_1` FOREIGN KEY (`AdmissionNo`) REFERENCES `Student` (`AdmissionNo`);
 
-  --
-  -- Constraints for table `Student_Subject_Grade`
-  --
-  ALTER TABLE `Student_Subject_Grade`
+--
+-- Constraints for table `Student_Subject_Grade`
+--
+ALTER TABLE `Student_Subject_Grade`
   ADD CONSTRAINT `Student_Subject_Grade_ibfk_1` FOREIGN KEY (`AdmissionNo`) REFERENCES `Student` (`AdmissionNo`);
 
   --
@@ -1845,3 +1867,4 @@ ALTER TABLE `LanguageOption`
   --
   ALTER TABLE `Transaction`
   ADD CONSTRAINT `Transaction_ibfk_1` FOREIGN KEY (`EventID`) REFERENCES `Event` (`EventID`);
+  COMMIT;
