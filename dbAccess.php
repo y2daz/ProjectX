bf6d94e77cbabc5ee76166bd0dc486f0a7ff9444
y2daz <yazdaan.alimudeen@gmail.com>
2014-09-08 20:56:32 +0530

    Working staffReport

--- dbAccess.php
+++ dbAccess.php
@@ -918,6 +918,7 @@ function getEventTransactions($eventid)
         return $set;
     }
 
+
     function deleteStaff($staffID)
     {
         $dbObj = new dbConnect();
@@ -1115,6 +1116,35 @@ function getEventTransactions($eventid)
         }
 
         if ($stmt = $mysqli->prepare("Select StaffID, NamewithInitials, NICNumber, ContactNumber FROM Staff WHERE isDeleted = 0 ORDER BY StaffId;"))
+        {
+            if ($stmt->execute())
+            {
+                $result = $stmt->get_result();
+                $i = 0;
+                while($row = $result->fetch_array())
+                {
+                    $set[$i++ ]=$row;
+                }
+            }
+        }
+
+        $mysqli->close();
+        return $set;
+    }
+
+    function getAllStaffDetailed()
+    {
+
+        $dbObj = new dbConnect();
+        $mysqli = $dbObj->getConnection();
+
+        $set = null;
+
+        if ($mysqli->connect_errno) {
+            die ("Failed to connect to MySQL: " . $mysqli->connect_error );
+        }
+
+        if ($stmt = $mysqli->prepare("Select * FROM Staff WHERE isDeleted = 0 ORDER BY StaffId;"))
         {
             if ($stmt->execute())
             {
--- staffManagement/staffReport.php
+++ staffManagement/staffReport.php
@@ -56,22 +56,22 @@ $column10Header = "මාස";
 $column1112Header = "මෙම විදුහලේ මන්වීම භාරගත් වර්ෂය හා මාසය ( අවසන් අංක දෙක )";
 $column11Header = $column9Header;
 $column12Header = $column10Header;
-$column13Header = "පාසලට පනවීමේ සවභාවය";
-$column14Header = "පැනවීමේ ලද මාද්‍යය";
-$column15Header = "පාසලේ  දුරන තනතුර";
-$column16Header = "ඉහලම අධ්‍යාපන් සුදුසුකම";
-$column17Header = "ඉහලම වෘත්තීය  සුදුසුකම";
-$column18Header = "වර්තමාන පත්වීමේ වර්ගීකරණය";
-$column19Header = "නිරතවන කාර්යය";
-$column20Header = "වැඩිම කාලයක් උගන්වන විෂයය";
-$column21Header = "දෙවනු වැඩි කාලයක් උගන්වන විෂයය";
+$column13Header = "පාසලට පනවීමේ සවභාවය"; //Employment Status
+$column14Header = "පැනවීමේ ලද මාද්‍යය"; //Medium
+$column15Header = "පාසලේ  දුරන තනතුර";  //Position in School
+$column16Header = "ඉහලම අධ්‍යාපන් සුදුසුකම"; //Highest Educational
+$column17Header = "ඉහලම වෘත්තීය  සුදුසුකම"; //Highest Professional
+$column18Header = "වර්තමාන පත්වීමේ වර්ගීකරණය"; //Course of study
+$column19Header = "නිරතවන කාර්යය"; //Section
+$column20Header = "වැඩිම කාලයක් උගන්වන විෂයය"; //Subj most taught
+$column21Header = "දෙවනු වැඩි කාලයක් උගන්වන විෂයය"; //Subj 2nd most taught
 
 $column222324Header = "2012.01.01 සිට 2012.12.31 දක්වා ලබාගෙන ඇති නිවාඩු ගණන ( පූර්ණ සංඛ්‍යාවලින් )";
-$column22Header = "අදාළ සේවය / ශ්‍රේනීය";
-$column23Header = "අනියම් / විවේක";
-$column24Header = "රාජකාරි / අධ්‍යයන";
-$column25Header = "ප්‍රසූත නිවාඩු";
-$column26Header = "2013 මැයි මස මුළු වැටුප ( සියලුම දීමනා ඇතුලත් කළ යුතු අතර අඩු කිරීම නොසලකන්න. පැරණි හිඟ මුදල් ශත ගණන් ඇතුලත් නොකරන්න)";
+$column22Header = "අදාළ සේවය / ශ්‍රේනීය"; //Service Grade
+$column23Header = "අනියම් / විවේක"; //Other Leave
+$column24Header = "රාජකාරි / අධ්‍යයන"; //Official
+$column25Header = "ප්‍රසූත නිවාඩු"; //Maternity
+$column26Header = "2013 මැයි මස මුළු වැටුප (සියලුම දීමනා ඇතුලත් කළ යුතු අතර අඩු කිරීම නොසලකන්න. පැරණි හිඟ මුදල් ශත ගණන් ඇතුලත් නොකරන්න)"; //salary
 $column27Header1 = "සාම නිලධාරියකු සඳහාම අනිවාර්යයෙන් ම ජාතික හැඳුනුම් පත් අංකය සඳහන් කල යුතුය";
 $column27Header2 = "ජාතික හැඳුනුම් පත් අංකය ";
 $column27Header3 = "උදා 583021007 V";
@@ -101,20 +101,57 @@ $column27Header3 = "උදා 583021007 V";
         h1, h2, h3{
             text-align: center;
         }
-        .report, th, td{
-            text-align: justify;
+        .report{
+        }
+        .report, .report th, .report td{
+            /*text-align: justify;*/
             border: 1px solid black;
             border-collapse: collapse;
-            padding-left: 4px;
-            padding-right: 4px;
+        }
+        .report .headerRow{
+            height:300px;
+        }
+        .report .salary{
+            text-align: right;
+        }
+        .numbers td{
+            text-align: center;
         }
         .numberCol{
-            /*width:20px;*/
+            text-align: left;
+            white-space: nowrap;
+            text-overflow: ellipsis;
+            min-width: 25px;
+            max-width: 25px;
+
+            writing-mode: bt-rl;
+            text-indent: -7em;
+            padding: 0px 0px 0px 0px;
+            margin: 0px;
         }
         #col_0{
+            max-width: 50px;
+            min-width: 50px;
         }
         #col_1{
-            width:300px;
+            max-width: 300px;
+            min-width: 300px;
+            padding-left: 10px;
+            padding-right: 10px;
+        }
+        #col_26{
+            max-width: 200px;
+            min-width: 200px;
+            padding-left: 8px;
+            padding-right: 8px;
+        }
+        #col_27{
+            text-align: justify;
+            padding-left: 14px;
+            padding-right: 14px;
+
+            max-width: 200px;
+            min-width: 200px;
         }
         .rotate { /*http://css-tricks.com/snippets/css/text-rotation/ */
             /* Safari */
@@ -128,11 +165,6 @@ $column27Header3 = "උදා 583021007 V";
             /* Internet Explorer */
             filter: progid:DXImageTransform.Microsoft.BasicImage(rotation=3);
 
-            writing-mode: bt-rl;
-            /*text-indent: -3em;*/
-            padding: 0px 0px 0px 0px;
-            margin: 0px;
-            text-align: left;
             /*vertical-align: top;*/
         }
     </style>
@@ -144,16 +176,122 @@ $column27Header3 = "උදා 583021007 V";
     <h3><?php echo $line2 ?></h3>
 
     <table class="report">
-        <tr>
-            <th id="col_0" class="rotate numberCol"><?php echo $column0Header ?></th>
-            <th id="col_1"><?php echo "<p>$column1Header1</p><p>$column1Header2</p>" ?></th>
-            <th id="col_2" class="rotate numberCol"><?php echo $column2Header ?></th>
-            <th id="col_3" class="rotate numberCol"><?php echo $column3Header ?></th>
-            <th id="col_4" class="rotate numberCol"><?php echo $column4Header ?></th>
+        <tr class="headerRow">
+            <td id="col_0" class="rotate numberCol"><?php echo $column0Header ?></td>
+            <td id="col_1"><?php echo "<p>$column1Header1</p><p>$column1Header2</p>" ?></td>
+            <td id="col_2" class="rotate numberCol"><?php echo $column2Header ?></td>
+            <td id="col_3" class="rotate numberCol"><?php echo $column3Header ?></td>
+            <td id="col_4" class="rotate numberCol"><?php echo $column4Header ?></td>
+            <td id="col_5" class="rotate numberCol"><?php echo $column5Header ?></td>
+            <td id="col_6" class="rotate numberCol"><?php echo $column6Header ?></td>
+            <td id="col_7" class="rotate numberCol"><?php echo $column7Header ?></td>
+            <td id="col_8" class="rotate numberCol"><?php echo $column8Header ?></td>
+            <td id="col_9" class="rotate numberCol"><?php echo $column9Header ?></td>
+            <td id="col_10" class="rotate numberCol"><?php echo $column10Header ?></td>
+            <td id="col_11" class="rotate numberCol"><?php echo $column11Header ?></td>
+            <td id="col_12" class="rotate numberCol"><?php echo $column12Header ?></td>
+            <td id="col_13" class="rotate numberCol"><?php echo $column13Header ?></td>
+            <td id="col_14" class="rotate numberCol"><?php echo $column14Header ?></td>
+            <td id="col_15" class="rotate numberCol"><?php echo $column15Header ?></td>
+            <td id="col_16" class="rotate numberCol"><?php echo $column16Header ?></td>
+            <td id="col_17" class="rotate numberCol"><?php echo $column17Header ?></td>
+            <td id="col_18" class="rotate numberCol"><?php echo $column18Header ?></td>
+            <td id="col_19" class="rotate numberCol"><?php echo $column19Header ?></td>
+            <td id="col_20" class="rotate numberCol"><?php echo $column20Header ?></td>
+            <td id="col_21" class="rotate numberCol"><?php echo $column21Header ?></td>
+            <td id="col_22" class="rotate numberCol"><?php echo $column22Header ?></td>
+            <td id="col_23" class="rotate numberCol"><?php echo $column23Header ?></td>
+            <td id="col_24" class="rotate numberCol"><?php echo $column24Header ?></td>
+            <td id="col_25" class="rotate numberCol"><?php echo $column25Header ?></td>
+            <td id="col_26" class="rotate"><?php echo $column26Header ?></td>
+            <td id="col_27" class="" colspan="10"><?php echo "<p>$column27Header1</p><p>$column27Header2</p><p>$column27Header3</p>"  ?></td>
         </tr>
+        <tr class="numbers">
+            <td></td>
+            <td>01</td>
+            <td>02</td>
+            <td>03</td>
+            <td>04</td>
+            <td>05</td>
+            <td>06</td>
+            <td>07</td>
+            <td>08</td>
+            <td>09</td>
+            <td>10</td>
+            <td>11</td>
+            <td>12</td>
+            <td>13</td>
+            <td>14</td>
+            <td>15</td>
+            <td>16</td>
+            <td>17</td>
+            <td>18</td>
+            <td>19</td>
+            <td>20</td>
+            <td>21</td>
+            <td>22</td>
+            <td>23</td>
+            <td>24</td>
+            <td>25</td>
+            <td>26</td>
+            <td>27</td>
+        </tr>
+
+        <?php
+
+        $result = getAllStaffDetailed();
+
+        if ($result == null)
+        {
+            echo "<tr><td colspan='37'>There are no records to show.</td></tr>";
+        }
+        else
+        {
+            foreach($result as $row){
+                echo "<tr>\n";
+                echo "<td>$row[0]</td>";
+
+                $date = strtotime($row[2]); //DOB
+                echo "<td>$row[1]</td>";
+                echo "<td>" . date("y",  $date) . "</td>";
+                echo "<td>" . date("m",  $date) . "</td>";
+                echo "<td>" . date("d",  $date) . "</td>";
+                echo "<td>$row[3]</td>"; //Gender
+                echo "<td>$row[4]</td>"; //Nat/Race
+                echo "<td>$row[5]</td>"; //Religion
+                echo "<td>$row[6]</td>"; //Civil Status
+                $date = strtotime($row[10]); //Date Appointed to Post
+                echo "<td>" . date("y",  $date) . "</td>";
+                echo "<td>" . date("m",  $date) . "</td>";
+                $date = strtotime($row[11]); //Date Appointed to Post
+                echo "<td>" . date("y",  $date) . "</td>";
+                echo "<td>" . date("m",  $date) . "</td>";
+                echo "<td>$row[12]</td>"; //Employment Status
+                echo "<td>$row[13]</td>"; //Medium
+                echo "<td>$row[14]</td>"; //Position in School
+                echo "<td>$row[20]</td>"; //Highest Educational Qualification
+                echo "<td>$row[21]</td>"; //Highest Professional Qualification
+                echo "<td>$row[22]</td>"; //Course of Study
+                echo "<td>$row[15]</td>"; //Section
+                echo "<td>$row[16]</td>"; //Subje Most
+                echo "<td>$row[17]</td>"; //Subj 2nd Most
+                echo "<td>$row[18]</td>"; //Service Grade
+                echo "<td>$row[0]</td>"; //Other Leave
+                echo "<td>$row[0]</td>"; //Offical Leave
+                echo "<td>$row[0]</td>"; //Maternity
+                echo "<td class='salary'>$row[19]</td>"; //Salary
+                echo "<td>$row[7]</td>";//NIC NUMBER
+                echo "</tr>\n";
+            }
+        }
+
+
+        ?>
+
 
     </table>
 
     <br />
+
 </body>
 </html>
