d0cba3c18e0389fdda945a2d40025157a9a78867
shavin47 <shavin47@hotmail.com>
2014-09-24 22:11:31 +0530

    check status

--- leaveManagement/checkStatus.php
+++ leaveManagement/checkStatus.php
@@ -1,16 +1,38 @@
 <?php
 
+define('THISROOT', $_SERVER['DOCUMENT_ROOT']);
+define('THISPATHFRONT', 'http://'.$_SERVER['HTTP_HOST']);
 
 require_once("../formValidation.php");
 require_once("../dbAccess.php");
+require_once(THISROOT . "/common.php");
 
-define('THISROOT', $_SERVER['DOCUMENT_ROOT']);
-define('THISPATHFRONT', 'http://'.$_SERVER['HTTP_HOST']);
+error_reporting(E_ERROR | E_PARSE);
+
 ob_start();
+
+$view = "none";
 
 if(isset($_POST["submit"]))
 {
+    $StaffID = $_POST["StaffID"];
 
+    if(is_numeric($StaffID))
+    {
+        if(checkStaffMember($StaffID))
+        {
+            $result = checkLeaveStatus($StaffID);
+            $view = "block";
+        }
+        else
+        {
+            sendNotification("Staff member does not exist");
+        }
+    }
+    else
+    {
+        sendNotification("Invalid Staff ID");
+    }
 }
 
 ?>
@@ -28,14 +50,35 @@ if(isset($_POST["submit"]))
 
             .checkStaff{
                 position: absolute;
-                left: 100px;
-                top: 100px;
+                left: 50px;
+                top: 110px;
             }
 
             .submitButton{
                 position: absolute;
-                left: 220px;
+                left: 170px;
                 top: 140px;
+            }
+
+            .viewTable{
+                position: absolute;
+                left: 230px;
+                top: 230px;
+                border-collapse: collapse;
+                display: <?php echo $view ?>;
+            }
+
+
+            .viewTable th{
+                align:center;
+                color:white;
+                background-color:#005e77;
+                height:25px;
+                padding:5px;
+            }
+
+            .viewTable td{
+                text-align: center;
             }
 
 
@@ -55,6 +98,69 @@ if(isset($_POST["submit"]))
         </table>
 
         <input type="submit" name="submit" value="Check" class="submitButton">
+
+        <table class="viewTable">
+
+            <th>Requested Date</th>
+            <th>Leave Type</th>
+            <th>Approval Status</th>
+
+            <?php
+
+            if(! isFilled($result))
+            {
+                echo "<tr><td colspan='3'>No request data</td></tr>";
+            }
+            else
+            {
+                $i = 1;
+
+                foreach($result as $row){
+                    $top = ($i++ % 2 == 0)? "<tr class=\"alt\">":"<tr>";
+
+                    $Status = "Pending";
+                    $LeaveType = "Other";
+                    $LeaveReason = $row[3];
+
+                    if($row[1] == 0)
+                    {
+                        $Status = "Pending";
+                    }
+                    else if ($row[1] == 1)
+                    {
+                        $Status = "Approved";
+                    }
+                    else if($row[1] == 2)
+                    {
+                        $Status = "Rejected";
+                    }
+
+                    if($row[2] == 1)
+                    {
+                        $LeaveType = "Official Leave";
+                    }
+                    else if($row[2] == 2)
+                    {
+                        $LeaveType = "Maternity Leave";
+                    }
+                    else if($row[2] == 3)
+                    {
+                        $LeaveType = "Other Leave";
+                    }
+
+
+                    echo $top;
+                    echo "<td> $row[0] </td>";
+                    echo "<td> $LeaveType </td>";
+                    echo "<td> $Status </td>";
+                    echo "</tr>";
+                }
+            }
+
+
+            ?>
+
+        </table>
 
     </form>
 
