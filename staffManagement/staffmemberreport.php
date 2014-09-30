fc42f1bdc218b3692235849a764922d4831e01cd
josa1992 <josephvimukthi@gmail.com>
2014-09-30 21:49:37 +0530

    staff member report

--- staffManagement/staffmemberreport.php
+++ staffManagement/staffmemberreport.php
@@ -54,7 +54,7 @@ if(isset($_GET["id"]))
         .stafftable
         {
             position: absolute;
-            left: 124px;
+            left: 125px;
             top:300px;
             width: 600px;
         }
