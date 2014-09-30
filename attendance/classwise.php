2508dd6cfa167242e6901d6b790e9a44d7a369b4
Ish777 <isurujkd@gmail.com>
2014-09-30 12:06:49 +0530

    final day

--- attendance/classwise.php
+++ attendance/classwise.php
@@ -149,7 +149,7 @@ $arrClassroom="";
                     <td><input class="button" type="submit" name="getAttendence" value="<?php echo $SubmitBtn?>"></td>
                     <td><a id="link"
                            href="<?php echo PATHFRONT . "/Attendance/reportClass.php" . "?grade=" . $_GET["studentGrade"] . "&dateFrom=" . $_GET["dateFrom"] ."&dateTo=" . $_GET["dateTo"] ?>"
-                           target="_blank" > Print Timetable</a></td>
+                           target="_blank" > Print Report</a></td>
                     <td></td>
                 </tr>
             </table>
