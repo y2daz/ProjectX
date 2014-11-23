39fbb621d643b94ecbf25b6759d7d8244e31a807
y2daz <yazdaan.alimudeen@gmail.com>
2014-11-23 18:51:49 +0530

    Tiny things.

--- administration/yearPlan.js
+++ administration/yearPlan.js
@@ -5,14 +5,14 @@
 
 function submitYearPlan()
 {
-    if ($('.selected').length > 0)
+    if ( $('.selected').length > 0)
     {
         var dates = new Array();
 
         $('.selected').each(function(i, obj)
         {
             var name = $(obj).attr('name');
-            dates.push(name);
+            dates.push( name );
             post(document.URL, dates, "POST");
         });
     }
